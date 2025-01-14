37bcb0503b28def0c378ea287e5d00fcfc63594f
MKH-MK <khametov.m@oundleschool.org.uk>
2025-01-14 10:23:22 +0000

    Sync

--- addstudents.php
+++ addstudents.php
@@ -7,17 +7,27 @@ array_map("htmlspecialchars", $_POST);
 print_r($_POST);
 echo($_POST["forename"]);
 
-$stmt = $conn->prepare("INSERT INTO tblstudents (StudentID,Gender,Surname,Forename,Password,House,Year ,Role)
-VALUES (null,:gender,:surname,:forename,:password,:house,:year,:role)");
+$Username=substr($_POST["forename"],0,1).".".$_POST["surname"];
+$hashed_password = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
-$stmt->bindParam(':forename', $_POST["forename"]);
-$stmt->bindParam(':surname', $_POST["surname"]);
-$stmt->bindParam(':house', $_POST["house"]);
+$stmt = $conn->prepare("INSERT INTO tblstudents (studentID,username,gender,surname,forename,password,house,yearg,role)
+VALUES (null,:username,:gender,:surname,:forename,:password,:house,:year,:role)");
+
+$stmt->bindParam(':un', $Username);
 $stmt->bindParam(':gender', $_POST["gender"]);
-$stmt->bindParam(':password', $_POST["passwd"]);
+$stmt->bindParam(':surname', $_POST["surname"]);
+$stmt->bindParam(':forename', $_POST["forename"]);
+$stmt->bindParam(':house', $_POST["house"]);
+$stmt->bindParam(':password', $_POST["password"]);
 $stmt->bindParam(':year', $_POST["year"]);
 
 $stmt->execute();
+
+catch(PDOException $e)
+{
+    echo "error".$e->getMessage();
+}
+
 $conn=null;
 
 ?>
--- install.php
+++ install.php
@@ -1,25 +1,45 @@
 <?php
 
-include_once("connection.php");
+$servername = "localhost";
+$username = "root";
+$password = "";
 
-// Create tblstudents table
-$stmt = $conn->prepare("DROP TABLE IF EXISTS tblstudents;
-CREATE TABLE tblstudents (
+try {
+    $conn = new PDO("mysql:host=$servername", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE DATABASE IF NOT EXISTS library";
+    $conn->exec($sql);
+
+    //next 3 lines optional only needed really if you want to go on an do more SQL here!
+    
+    $sql = "USE library";
+    $conn->exec($sql);
+    echo "DB created successfully";
+
+    // Create tblstudents table
+
+    $stmt = $conn->prepare("DROP TABLE IF EXISTS tblstudents;
+
+    CREATE TABLE tblstudents (
     studentid INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    gender VARCHAR(1) NOT NULL,
+    username VARCHAR(20) NOT NULL,
     surname VARCHAR(20) NOT NULL,
     forename VARCHAR(20) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    gender VARCHAR(1) NOT NULL,
+    house VARCHAR(20) NOT NULL,
     yearg INT(2) NOT NULL,
     UNIQUE KEY surname_forename (surname, forename)
-);");
-$stmt->execute();
-$stmt->closeCursor();
-echo "<br>tblstudents created";
+    );");
 
-// Create tblbooks table
-$stmt = $conn->prepare("DROP TABLE IF EXISTS tblbooks;
-CREATE TABLE tblbooks (
+    $stmt->execute();
+    $stmt->closeCursor();
+    echo "<br>tblstudents created";
+
+
+    // Create tblbooks table
+    $stmt = $conn->prepare("DROP TABLE IF EXISTS tblbooks;
+    CREATE TABLE tblbooks (
     bookid INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     isbn VARCHAR(13) NOT NULL,
     title VARCHAR(255) NOT NULL,
@@ -27,30 +47,43 @@ CREATE TABLE tblbooks (
     cover VARCHAR(255) NOT NULL,
     blurb TEXT NOT NULL,
     rating INT(1) NOT NULL
-);");
-$stmt->execute();
-$stmt->closeCursor();
-echo "<br>tblbooks created";
+    );");
 
-// Create tblstudentloans table
-$stmt = $conn->prepare("DROP TABLE IF EXISTS tblstudentloans;
-CREATE TABLE tblstudentloans (
+    $stmt->execute();
+    $stmt->closeCursor();
+    echo "<br>tblbooks created";
+
+    // Create tblstudentloans table
+
+    $stmt = $conn->prepare("DROP TABLE IF EXISTS tblstudentloans;
+
+    CREATE TABLE tblstudentloans (
     bookid INT(5) NOT NULL,
     userid INT(5) NOT NULL,
     endloan DATE NOT NULL,
     classgrade CHAR(1) NOT NULL,
     PRIMARY KEY(bookid, userid),
     UNIQUE KEY studentid_bookid (bookid, userid)
-);");
-$stmt->execute();
-$stmt->closeCursor();
-echo "<br>tblstudentloans created";
+    );");
 
-// Insert initial data into tblstudents
-$hashed_password = password_hash("password", PASSWORD_DEFAULT);
-$stmt = $conn->prepare("INSERT INTO tblstudents (studentid, gender, surname, forename, password, house, yearg) VALUES 
-    (NULL, 'M', 'Khametov', 'Mark', :hp, 'Crosby', 12)");
-$stmt->bindParam(':hp', $hashed_password);
-$stmt->execute();
-$stmt->closeCursor();
+    $stmt->execute();
+    $stmt->closeCursor();
+    echo "<br>tblstudentloans created";
+
+    // Insert initial data into tblstudents
+
+    $hashed_password = password_hash("password", PASSWORD_DEFAULT);
+    $stmt = $conn->prepare("INSERT INTO tblstudents (studentid, username, gender, surname, forename, password, house, yearg) VALUES 
+    (NULL, 'mark.k', 'M', 'Khametov', 'Mark', :hp, 'Crosby', 12)");
+   
+    $stmt->bindParam(':hp', $hashed_password);
+    $stmt->execute();
+    // $stmt->closeCursor();
+
+}
+catch(PDOException $e)
+{
+    echo $sql . "<br>" . $e->getMessage();
+}
+$conn=Null;
 ?>
